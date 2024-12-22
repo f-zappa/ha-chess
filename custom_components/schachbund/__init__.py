29cbc4b86388f90890b5c9b39958dc98618a934a
Uli Baumann <git@uli-baumann.de>
2024-12-22 17:18:02 +0100

    move from async to sync io

--- custom_components/schachbund/__init__.py
+++ custom_components/schachbund/__init__.py
@@ -1,17 +1,17 @@
 DOMAIN = "schachbund"
 
 
-async def async_setup(hass, config):
+def setup(hass, config):
     return True
 
 
-async def async_setup_entry(hass, entry):
+def setup_entry(hass, entry):
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
 
 
-async def async_unload_entry(hass, entry):
+def unload_entry(hass, entry):
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     return True
