07ad6ccff359bffb69b929bc5f86d316e22cd4ba
Uli Baumann <git@uli-baumann.de>
2024-12-22 17:32:23 +0100

    bugfixes

--- custom_components/schachbund/__init__.py
+++ custom_components/schachbund/__init__.py
@@ -1,18 +1,17 @@
 DOMAIN = "schachbund"
 
 
-def setup(hass, config):
+def async_setup(hass, config):
     return True
 
 
-# def setup_entry(hass, entry):
-#     hass.async_create_task(
-#         hass.config_entries.async_forward_entry_setup(entry, "sensor")
-#     )
-#     return True
-# 
-# 
-# def unload_entry(hass, entry):
-#     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-#     return True
-# 
+def async_setup_entry(hass, entry):
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+
+def async_unload_entry(hass, entry):
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return True
